ac2b9c73dd5bf0bf610a0e660d4ccd8280887951
jyjylow <jiayi0457@gmail.com>
2025-05-23 14:40:39 +0100

    added tomahawk data

---
+++ main.py
@@ -0,0 +1,15 @@
+import marimo
+
+__generated_with = "0.13.7"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import pandas as pd
+    import pyarrow as pa
+    return
+
+
+if __name__ == "__main__":
+    app.run()
